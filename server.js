4bba2aab74eacdaf5829a6d0457ed55e24a578c2
Philip Lin <philipo227@hotmail.com>
2016-11-07 00:51:20 -0800

    setting up serverjs

---
+++ server.js
@@ -0,0 +1,40 @@
+var
+  express = require('express'),
+  app = express(),
+  mongoose = require('mongoose'),
+  morgan = require('morgan'),
+  bodyParser = require('body-parser'),
+  methodOverride = require('method-override'),
+  bower = require('bower'),
+  PORT = process.env.PORT || 3000
+
+  //EXPRESS CONFIGURATION
+  // ----------------------
+  //set the MongoDB connection
+  mongoose.Promise = global.Promise
+  mongoose.connect('mongodb:/localhost/MeanMapApp', function(err){
+    if(err) throw err
+    console.log("connected! Sweet! :]");
+  })
+
+  //Logging and Parsing
+
+  app.use(express.static( __dirname +'/public')) // sets the static files location to public
+  app.use('/bower_components', express.static(__dirname + '/bower_components')) // Use BowerComponents
+  app.use(morgan('dev')) // log with Morgan
+  app.use(bodyParser.json()) // parse application/json
+  app.use(bodyParser.urlencoded({extended:true})) // parse application/x-www-form-urlencoded
+  app.use(bodyParser.text())  // allows bodyParser to look at raw text
+  app.use(bodyParser.json({ type: 'application/vnd.api +json'}))  // parse application/vnd.api+json as json
+  app.use(methodOverride())
+
+
+  //Routes
+  //---------------
+  require('./app/routes.js')(app)
+
+
+
+  app.listen(PORT, function(err){
+    console.log(err || "server running on " + PORT);
+  })
